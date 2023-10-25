3fc507279c02d2a81a69067ae6af4a3e3b6c93d1
bechir105 <mohamedbechir.kefi@esprit.tn>
2023-10-25 20:29:40 +0100

    Initial Commit

---
+++ Wc2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wc2022
+//
+//  Created by Bechir Kefi on 25/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
